e8a8127d669286f4274c12fa0d280ae4aaddc990
Anatol <58941723+ussury@users.noreply.github.com>
2020-09-26 14:16:45 +0300

    Новая редакция файла

--- multilingual_online_translator.py
+++ multilingual_online_translator.py
@@ -2,44 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 
-print("""Hello, you're welcome to the translator.
-Translator supports: 
-1. Arabic
-2. German
-3. English
-4. Spanish
-5. French
-6. Hebrew
-7. Japanese
-8. Dutch
-9. Polish
-10. Portuguese
-11. Romanian
-12. Russian
-13. Turkish
-""")
+all_languages = ['arabic', 'german', 'english', 'spanish', 'french', 'hebrew', 'japanese', 'dutch', 'polish', 'portuguese', 'romanian', 'russian', 'turkish']
 
-lang_dict = {
-    '1': 'arabic',
-    '2': 'german',
-    '3': 'english',
-    '4': 'spanish',
-    '5': 'french',
-    '6': 'hebrew',
-    '7': 'japanese',
-    '8': 'dutch',
-    '9': 'polish',
-    '10': 'portuguese',
-    '11': 'romanian',
-    '12': 'russian',
-    '13': 'turkish'
-}
+print('Hello, you\'re welcome to the translator. Translator supports:')
+for position, val in enumerate(all_languages, 1):
+    print(str(position) + '.', val.capitalize())
 
-your_lang = input('Type the number of your language: \n')
-translate_lang = input('Type the number of language you want to translate to: \n')
+your_lang = int(input('Type the number of your language: \n'))
+translate_lang = int(input('Type the number of a language you want to translate to or "0" to translate to all languages: \n'))
 word = input('Type the word you want to translate: \n')
-_url = f'https://context.reverso.net/translation/{lang_dict[your_lang]}-{lang_dict[translate_lang]}/{word}'
-
+_url = f'https://context.reverso.net/translation/{all_languages[your_lang - 1]}-{all_languages[translate_lang - 1]}/{word}'
 
 headers = {'user-agent': 'Mozilla/5.0'}
 r = requests.get(_url, headers=headers)
@@ -49,31 +21,25 @@ soup = BeautifulSoup(r.content, 'html.parser')
 
 def words_translation(_soup):
     words_trans = _soup.select('#translations-content .translation')
-    words_trans_list = []
-    for item in words_trans:
-        words_trans_list.append(item.text.strip())
+    words_trans_list = [item.text.strip() for item in words_trans]
 
     for i in words_trans_list[:5]:
         print(i)
 
 
 def examples_content(_soup):
-    _examples_content = _soup.select('#examples-content .example')
-    _examples_content_list = []
-    for item in _examples_content:
-        _examples_content_list.append(item.get_text(strip=True))
+    _examples_content = _soup.select('#examples-content .text')
+    _examples_content_list = [item.text.strip('\n " " []') for item in _examples_content]
 
-    for i in _examples_content_list[:5]:
-        print(i + '\n')
+    print("\n\n".join(("\n".join(j for j in _examples_content_list[i:i+2]) for i in range(0,10,2))))
 
 
 def translation(_soup):
-    print(f'\n{lang_dict[translate_lang]} Translations:')
+    print(f'\n{all_languages[translate_lang - 1].capitalize()} Translations:')
     words_translation(_soup)
-    print(f'\n{lang_dict[translate_lang]} Examples:')
+    print(f'\n{all_languages[translate_lang - 1].capitalize()} Examples:')
     examples_content(_soup)
 
 
 translation(soup)
-
 
